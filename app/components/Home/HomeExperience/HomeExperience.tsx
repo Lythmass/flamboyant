ef0c989739f04b4e240516683da86de623fee075
Lythmass <lythmass17@gmail.com>
2023-07-18 14:58:35 +0400

    style: change experience styles

--- app/components/Home/HomeExperience/HomeExperience.tsx
+++ app/components/Home/HomeExperience/HomeExperience.tsx
@@ -2,8 +2,8 @@ import { HomeExperienceType } from 'types';
 
 export const HomeExperience: React.FC<HomeExperienceType> = (props) => {
   return (
-    <div className='w-1/4 text-dark bg-light h-full border-2 border-dark flex justify-center items-center shadow-[0.4rem_0.4rem_var(--accent-1)] hover:shadow-[0.8rem_0.8rem_var(--accent-1)] hover:translate-x-[-0.4rem] hover:translate-y-[-0.4rem] hover:bg-accent-2 hover:text-light transition-all'>
-      <h1 className='text-4xl text-center capitalize'>
+    <div className='w-1/4 boxes text-dark bg-accent-1 h-full border-2 border-dark flex justify-center items-center shadow-[0.4rem_0.4rem_var(--dark)] hover:shadow-[0.8rem_0.8rem_var(--dark)] hover:translate-x-[-0.4rem] hover:translate-y-[-0.4rem] transition-all'>
+      <h1 className='text-4xl font-bold text-center capitalize'>
         {props.quantity}+ {props.topic}
       </h1>
     </div>
