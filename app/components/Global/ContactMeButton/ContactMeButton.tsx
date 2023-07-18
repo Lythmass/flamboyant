79ead2ecc41b551c6521acd15341365211238bdf
Lythmass <lythmass17@gmail.com>
2023-07-18 14:30:34 +0400

    style: add background effect

--- app/components/Global/ContactMeButton/ContactMeButton.tsx
+++ app/components/Global/ContactMeButton/ContactMeButton.tsx
@@ -4,7 +4,7 @@ export const ContactMeButton = () => {
   return (
     <Link
       href='#contact'
-      className='px-3 text-dark py-2 border-2 border-dark text-2xl hover:shadow-[0_0_var(--accent-2)] hover:translate-x-[0.4rem] hover:translate-y-[0.4rem] shadow-[0.4rem_0.4rem_var(--accent-2)] transition-all'
+      className='px-3 bg-light text-dark py-2 border-2 border-dark text-2xl hover:shadow-[0_0_var(--accent-2)] hover:translate-x-[0.4rem] hover:translate-y-[0.4rem] shadow-[0.4rem_0.4rem_var(--accent-2)] transition-all'
     >
       Reach out to me
     </Link>
--- app/components/Home/HomeExperience/HomeExperience.tsx
+++ app/components/Home/HomeExperience/HomeExperience.tsx
@@ -2,7 +2,7 @@ import { HomeExperienceType } from 'types';
 
 export const HomeExperience: React.FC<HomeExperienceType> = (props) => {
   return (
-    <div className='w-1/4 text-dark h-full border-2 border-dark flex justify-center items-center shadow-[0.4rem_0.4rem_var(--accent-1)] hover:bg-accent-2 hover:text-light transition-all'>
+    <div className='w-1/4 text-dark bg-light h-full border-2 border-dark flex justify-center items-center shadow-[0.4rem_0.4rem_var(--accent-1)] hover:shadow-[0.8rem_0.8rem_var(--accent-1)] hover:translate-x-[-0.4rem] hover:translate-y-[-0.4rem] hover:bg-accent-2 hover:text-light transition-all'>
       <h1 className='text-4xl text-center capitalize'>
         {props.quantity}+ {props.topic}
       </h1>
