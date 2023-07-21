9893be0ebd88f463f64f100354f32dc7c1eb8240
Lythmass <lythmass17@gmail.com>
2023-07-21 18:55:07 +0400

    style: update responsiveness

--- app/components/Services/Service/Service.tsx
+++ app/components/Services/Service/Service.tsx
@@ -11,19 +11,19 @@ export const Service: React.FC<ServiceType> = (props) => {
   return (
     <div
       style={{ backgroundColor: props.backgroundColor }}
-      className={`w-full min-h-[27rem] p-10 flex flex-col gap-1 ${isOddAndLast}`}
+      className={`w-full min-h-[27rem] p-3 lg:p-10 flex flex-col gap-1 ${isOddAndLast}`}
     >
       <div className='w-full flex flex-col-reverse lg:flex-row lg:items-center gap-7 lg:gap-0 lg:justify-between'>
         <h1 className='text-dark font-bold text-4xl'>{props.title}</h1>
-        <div className={`${props.animatedIcon} relative`} />
+        <div className={`${props.animatedIcon} relative lg:m-0 mt-5 ml-5`} />
       </div>
       <p
-        className={`${fontSecondary.className} font-medium text-lg pr-10 text-dark`}
+        className={`${fontSecondary.className} font-medium lg:text-lg lg:pr-10 text-dark`}
       >
         {props.shortDescription}
       </p>
       <ul
-        className={`${fontSecondary.className} text-dark text-2xl flex flex-col gap-1 mt-7 font-medium`}
+        className={`${fontSecondary.className} text-dark text-lg lg:text-2xl flex flex-col gap-1 mt-7 font-medium`}
       >
         {displaySubServices}
       </ul>
