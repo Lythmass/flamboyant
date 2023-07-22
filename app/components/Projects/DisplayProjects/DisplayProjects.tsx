ca2faaac9e28001711a495da8129c04525c8a141
Lythmass <lythmass17@gmail.com>
2023-07-22 12:20:46 +0400

    feat: add slider to projects

--- app/components/Projects/DisplayProjects/DisplayProjects.tsx
+++ app/components/Projects/DisplayProjects/DisplayProjects.tsx
@@ -1,23 +1,61 @@
 'use client';
 import { ProjectsData } from 'data';
 import { Project } from 'components';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { motion } from 'framer-motion';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-creative';
+import { Pagination, EffectCreative } from 'swiper/modules';
 
 export const DisplayProjects = () => {
   const displayProjects = ProjectsData.map((project) => {
     return (
-      <Project
-        key={project.id}
-        title={project.title}
-        description={project.description}
-        image={project.image}
-        link={project.link}
-        technologies={project.technologies}
-        startDate={project.startDate}
-        endDate={project.endDate}
-      />
+      <SwiperSlide key={project.id}>
+        <Project
+          title={project.title}
+          description={project.description}
+          image={project.image}
+          link={project.link}
+          technologies={project.technologies}
+          startDate={project.startDate}
+          endDate={project.endDate}
+        />
+      </SwiperSlide>
     );
   });
-  return <>{displayProjects}</>;
+  return (
+    <motion.div
+      initial={{ opacity: 1, y: 150 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ delay: 0.1, duration: 1.25, type: 'spring' }}
+      className='w-full'
+    >
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        effect={'creative'}
+        loop
+        modules={[Pagination, EffectCreative]}
+        creativeEffect={{
+          prev: {
+            shadow: true,
+            translate: ['-100%', 0, -1],
+          },
+          next: {
+            translate: ['100%', 0, 0],
+          },
+        }}
+        className='w-full'
+        spaceBetween={400}
+        slidesPerView={1}
+      >
+        {displayProjects}
+      </Swiper>
+    </motion.div>
+  );
 };
 
 export default DisplayProjects;
--- app/components/Projects/Project/Project.tsx
+++ app/components/Projects/Project/Project.tsx
@@ -1,18 +1,10 @@
-'use client';
 import Image from 'next/image';
 import { ProjectDescription, ProjectDetails } from 'components';
 import { ProjectType } from 'types';
-import { motion } from 'framer-motion';
 
 export const Project: React.FC<ProjectType> = (props) => {
   return (
-    <motion.div
-      initial={{ opacity: 1, y: 150 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true }}
-      transition={{ delay: 0.1, duration: 1.25, type: 'spring' }}
-      className='w-full boxes shadow-[0.4rem_0.4rem_var(--dark)] p-3 lg:p-7 border-2 border-dark pb-5 flex flex-col lg:flex-row items-start gap-10 lg:gap-0'
-    >
+    <div className='w-full boxes shadow-[0.4rem_0.4rem_var(--dark)] p-3 lg:p-7 border-2 border-dark pb-5 flex flex-col lg:flex-row items-start gap-10 lg:gap-0'>
       <div className='w-full lg:w-1/2 min-h-[31rem] flex flex-col items-start gap-1'>
         <ProjectDescription
           technologies={props.technologies}
@@ -32,7 +24,7 @@ export const Project: React.FC<ProjectType> = (props) => {
         height={700}
         alt={props.title}
       />
-    </motion.div>
+    </div>
   );
 };
 
