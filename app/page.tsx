30d74a57b94591eccbbe53a7e9497c7bda76eb1d
Lythmass <lythmass17@gmail.com>
2023-07-23 12:14:17 +0400

    fix: footer height

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Main() {
       <Projects />
       <Services />
       <Contact />
-      <footer className='w-full lg:px-10 gap-4 flex-col lg:flex-row bg-dark h-[9rem] p-7 flex justify-start lg:justify-between items-start lg:items-center'>
+      <footer className='w-full lg:px-10 gap-4 flex-col lg:flex-row bg-dark min-h-[5rem] p-7 flex justify-start lg:justify-between items-start lg:items-center'>
         <h1
           className={`text-accent-2 w-full text-xl font-bold ${fontSecondary.className}`}
         >
