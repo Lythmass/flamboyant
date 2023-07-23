5889eed2a01bc370ff60953810d5b2dbbd31db1d
Lythmass <lythmass17@gmail.com>
2023-07-23 11:28:24 +0400

    style: add first footer design

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,9 @@ export default function Main() {
       <Projects />
       <Services />
       <Contact />
+      <footer className='w-full bg-dark h-[9rem] p-7'>
+        <h1 className='text-accent-1 text-3xl font-bold'>Copyright Lythmass</h1>
+      </footer>
     </main>
   );
 }
