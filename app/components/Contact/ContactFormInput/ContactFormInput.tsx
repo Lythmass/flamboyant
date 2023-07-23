25219e1c4342eef204f86d4b18dcd9bb852e80cd
Lythmass <lythmass17@gmail.com>
2023-07-23 21:12:42 +0400

    style: shrink texts in contact for mobile

--- app/components/Contact/ContactFormInput/ContactFormInput.tsx
+++ app/components/Contact/ContactFormInput/ContactFormInput.tsx
@@ -5,13 +5,13 @@ import { InputType } from 'types';
 export const ContactFormInput: React.FC<InputType> = (props) => {
   return (
     <div className='w-full flex flex-col gap-1'>
-      <label htmlFor={props.name} className='text-2xl font-bold'>
+      <label htmlFor={props.name} className='text-lg lg:text-2xl font-bold'>
         {props.label}
       </label>
       {props.type !== 'textarea' && (
         <input
           {...props.register(props.name, FormValidation[props.name])}
-          className={`w-full py-2 ${fontSecondary.className} text-xl px-3 bg-light border-2 border-dark focus:shadow-[0.4rem_0.4rem_var(--accent-1)] focus:outline-none text-dark font-semibold transition-all`}
+          className={`w-full py-2 ${fontSecondary.className} lg:text-xl px-3 bg-light border-2 border-dark focus:shadow-[0.4rem_0.4rem_var(--accent-1)] focus:outline-none text-dark font-semibold transition-all`}
           id={props.name}
           type={props.type}
           placeholder={props.placeholder}
@@ -23,7 +23,7 @@ export const ContactFormInput: React.FC<InputType> = (props) => {
           {...props.register(props.name, FormValidation[props.name])}
           id={props.name}
           placeholder={props.placeholder}
-          className={`w-full h-[10rem] py-2 ${fontSecondary.className} text-xl px-3 bg-light border-2 border-dark focus:shadow-[0.4rem_0.4rem_var(--accent-1)] focus:outline-none text-dark font-semibold transition-all`}
+          className={`w-full h-[10rem] py-2 ${fontSecondary.className} lg:text-xl px-3 bg-light border-2 border-dark focus:shadow-[0.4rem_0.4rem_var(--accent-1)] focus:outline-none text-dark font-semibold transition-all`}
         />
       )}
       <p
