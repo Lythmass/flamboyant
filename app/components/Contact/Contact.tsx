cd5b47fbfbc53296ee9e4ac4e14962917379e749
Lythmass <lythmass17@gmail.com>
2023-07-23 11:54:42 +0400

    tryfix: try to fix mobile overflow in bottom

--- app/components/Contact/Contact.tsx
+++ app/components/Contact/Contact.tsx
@@ -4,7 +4,7 @@ export const Contact = () => {
   return (
     <div
       id='contact'
-      className='w-full relative px-5 2xl:px-32 lg:py-24 py-12 flex flex-col gap-16'
+      className='w-full relative overflow-hidden px-5 2xl:px-32 lg:py-24 py-12 flex flex-col gap-16'
     >
       <SectionTitle title='Contact Me' />
       <div className='w-full flex gap-8 flex-col-reverse 1330:flex-row'>
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import { About, Contact, Home, Projects, Services } from 'components';
 
 export default function Main() {
   return (
-    <main className='w-full h-full overflow-x-hidden scroll-smooth'>
+    <main className='w-full h-full overflow-auto overflow-x-hidden scroll-smooth'>
       <Home />
       <About />
       <Projects />
       <Services />
       <Contact />
-      <footer className='w-full overflow-hidden bg-dark h-[9rem] p-7'>
+      <footer className='w-full bg-dark h-[9rem] p-7'>
         <h1 className='text-accent-1 text-3xl font-bold'>Copyright Lythmass</h1>
       </footer>
     </main>
