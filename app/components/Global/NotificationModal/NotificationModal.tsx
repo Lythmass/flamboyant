39a9c32d394fa8c0f63f7decb18591c24c9a3573
Lythmass <lythmass17@gmail.com>
2023-07-23 20:58:40 +0400

    style: make modal responsive

--- app/components/Global/NotificationModal/NotificationModal.tsx
+++ app/components/Global/NotificationModal/NotificationModal.tsx
@@ -10,9 +10,11 @@ export const NotificationModal: React.FC<NotificationModalType> = (props) => {
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       ref={scope}
-      className='w-[30rem] p-5 min-h-[7rem] shadow-[0.4rem_0.4rem_var(--accent-1)] flex items-center justify-start absolute z-[500] top-10 right-10 bg-dark'
+      className='sm:w-[30rem] w-[90%] p-5 min-h-[7rem] shadow-[0.4rem_0.4rem_var(--accent-1)] flex items-center justify-start absolute z-[500] top-3 right-[5%] md:translate-x-0 md:top-10 md:right-10 bg-dark'
     >
-      <h1 className='text-accent-2 font-bold text-3xl'>{props.text}</h1>
+      <h1 className='text-accent-2 font-bold text-lg lg:text-3xl'>
+        {props.text}
+      </h1>
     </motion.div>
   );
 };
