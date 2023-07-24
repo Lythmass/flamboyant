26370d0dba51e5d3aa038baf8ffeeadcfdb858f7
Lythmass <lythmass17@gmail.com>
2023-07-24 13:17:30 +0400

    fix: testimonials padding

--- app/components/Clients/Clients.tsx
+++ app/components/Clients/Clients.tsx
@@ -4,7 +4,7 @@ export const Clients = () => {
   return (
     <div
       id='clients'
-      className='w-full relative px-1 lg:px-5 2xl:px-32 lg:py-24 py-12 min-h-screen flex flex-col gap-16'
+      className='w-full relative px-5 2xl:px-32 lg:py-24 py-12 min-h-screen flex flex-col gap-16'
     >
       <SectionTitle title='Testimonials' />
       <div className='w-full flex flex-col gap-5'>
